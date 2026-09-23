a519a99fe5871a0cfd6788efe64bdbe66cea70a9
Lanazareth <lauragoncalvesnazareth@gmail.com>
2026-09-23 08:52:44 -0300

    23/09/26

---
+++ AV_BeckEnd/rec01_producao.js
@@ -0,0 +1,11 @@
+ console.log("=== Producção Diaria de Embalagens ===");
+
+ const caixasPorHora = 75;
+ const HorasTrabalhadas = 8;
+
+ const total = caixasPorHora * HorasTrabalhadas;
+
+ console.log(`Quantidade de caixas por Hora: ${caixasPorHora}`);
+ console.log(` Quantidade de Horas trabalhadas: ${HorasTrabalhadas}`);
+ console.log(`O total produzido foi de: ${total}`);
+ console.log("-".repeat(30));
---
+++ AV_BeckEnd/rec02_materiais.js
@@ -0,0 +1,14 @@
+const entrada = require(`redline-sync`);
+
+const nomePeca = entrada.question(" Digite o nome da Peça: ");
+const Qntdd = entrada.questionInt(" Digite a Quantidsade comprada: ");
+const valorUnitario = entrada.questionFloat(" Digite o valor unitario da Peça: ");
+
+const totalCompra = Qntdd * valorUnitario;
+
+console.log("=== RESUMO DA COMPRA ===");
+console.log(`Peça comprada: ${nomePeca}`);
+console.log(`A quantidade comprada foi de: ${Qntdd}`);
+console.log(`O valor unitario e de : ${valorUnitario}`);
+console.log(` O total da compra e de: ${totalCompra}`);
+console.log("-".repeat(30));
